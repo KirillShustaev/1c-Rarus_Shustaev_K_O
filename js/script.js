1a947500ba89ad81f581a16975ad7a7db8f39901
Kirill Shustaev <kirill.shustaev@yandex.ru>
2024-09-12 23:03:01 +0300

    desktop

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let navLink = document.querySelectorAll(".nav__link")
+
+navLink.forEach(elem => {
+    elem.addEventListener("click", () => {
+        for(e of navLink) {
+            e.classList.remove("activeNav")
+        }
+        elem.classList.add("activeNav")
+    })
+})
+
+swiper = new Swiper('.nav', {
+	slidesPerGroup: 1,
+    slidesPerView: "auto",
+	spaceBetween: 37,
+	loop: true,
+	navigation: {
+		nextEl: '.nav__next'
+		},
+  })
